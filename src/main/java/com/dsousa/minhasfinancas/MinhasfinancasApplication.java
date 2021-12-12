f4db3fee0321c8aae06957e7866961b39e12839f
Matheus <matheus@lexp.com.br>
2021-12-12 15:46:44 -0300

    final

--- src/main/java/com/dsousa/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/dsousa/minhasfinancas/MinhasfinancasApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MinhasfinancasApplication implements WebMvcConfigurer {	
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
